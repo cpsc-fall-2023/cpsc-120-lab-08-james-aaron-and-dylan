142c842483ab318eb632e87c66d680f8e0ca5333
lejames11367 <lejames11367@csu.fullerton.edu>
2023-11-01 20:20:48 -0700

    finished part 1

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -8,18 +8,16 @@
 #include <vector>
 
 int main(int argc, char* argv[]) {
-  int main(int argc, char* argv[]) {
-    std::vector<std::string> arguments(argv, argv + argc);
-    if (arguments.size() != 4) {
-      std::cout << "error: you must supply three arguments";
-      return -1;
-    }
-    std::string protein = arguments.at(1);
-    std::string bread = arguments.at(2);
-    std::string condiment = arguments.at(3);
-    std::cout << "Your order:\n"
-              << "A " << protein << " sandwich on " << bread << " with "
-              << condiment;
-    return 0;
+  std::vector<std::string> arguments(argv, argv + argc);
+  if (arguments.size() != 3) {
+    std::cout << "error: you must supply three arguments";
+    return -1;
   }
+  std::string protein = arguments.at(1);
+  std::string bread = arguments.at(2);
+  std::string condiment = arguments.at(3);
+  std::cout << "Your order:\n"
+            << "A " << protein << " sandwich on " << bread << " with "
+            << condiment;
+  return 0;
 }
